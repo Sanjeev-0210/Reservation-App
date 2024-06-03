767320ba1a5dc311560d24bb1dc57c55debc990b
Sanjeev C <sanjeev0210c@gmail.com>
2024-06-03 22:46:09 +0530

    Added Footer Section

--- reservationapp-fe/src/Components/AdminFooter.jsx
+++ reservationapp-fe/src/Components/AdminFooter.jsx
@@ -1,9 +1,23 @@
+import { Link } from 'react-router-dom';
+import '../Styles/AdminFooter.css'
 const AdminFooter = () => {
-    return ( 
+    return (
         <div className="adminfooter">
-            <h3>Admin Footer</h3>
+            <div className='links'>
+                <div id='about'>
+                    <Link to='/adminhomepage'> <h2><span>go</span>ibibo<sup>.in</sup></h2></Link>
+                    <p>gobibio is the world's largest online bus ticket booking service trusted by over 
+                        25 million happy customers globally. gobibio offers bus ticket booking through its website, 
+                        iOS and Android mobile apps for all major routes.</p>
+                </div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='rights'></div>
         </div>
-     );
+    );
 }
- 
+
 export default AdminFooter;
